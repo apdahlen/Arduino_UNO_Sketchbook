2a04071734ef1d42dc8c90604cad8cd39fbf761e
Aaron Dahlen <APDahlen@gmail.com>
2015-02-15 21:51:13 -0500

    working on MODBUS library

---
+++ libraries/ASCII_MODBUS/ASCII_MOSBUS_master.h
@@ -0,0 +1,37 @@
+
+
+This code is under development...
+
+#ifndef _MODBUS_support
+
+    #define _MODBUS_support
+
+
+// MASTER
+
+    #define MODBUS_READ             0x03
+    #define MODBUS_WRITE            0x06
+
+    #define BUS_WRITE               0x01
+    #define BUS_READ                0x00
+
+    #define USART_TIMEOUT_MILLISECONDS  100
+
+    #define size_of_cmd_lines           40
+
+    extern char MODBUS_cmd_line[size_of_cmd_lines];
+    extern char MODBUS_reply_line[size_of_cmd_lines];
+
+
+    uint8_t MODBUS_put_word(uint8_t physical_addr, uint16_t mem_addr, uint16_t data);
+    uint8_t MODBUS_read_registers(uint16_t *destination, uint8_t physical_addr, uint16_t starting_mem_addr, uint16_t get_n_words );
+
+// SLAVE
+
+    #define MODBUS_STR_LENGTH       100
+
+
+
+    // FIXME is would also be nice to have a put multiple words
+
+#endif
---
+++ libraries/error/errors.h
@@ -0,0 +1,21 @@
+#ifndef _ERRORS
+
+    #define _ERRORS
+
+    #define SIZE_ERROR_MSG 40
+
+    extern char ERROR_MSG[SIZE_ERROR_MSG];
+
+    #ifdef SOFTWARE_SERIAL_DBG
+
+        #include <SoftwareSerial.h>
+
+        SoftwareSerial bit_bang_serial(10, 11); // RX, TX
+
+    #endif
+
+
+    mySerial.println("Hello, world?");
+
+
+#endif
